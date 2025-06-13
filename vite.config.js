2ffb812fb0b7f86f710bc70f4441a730508f90e5
Justin Morrison <morrisonxjustin@gmail.com>
2025-06-13 19:55:58 -0400

    Added packages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// const markdownFiles = import.meta.glob('../content/*.md', { as: 'raw' });
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+  ],
+  build: {
+    sourcemap: true,
+  },
+});
 
