5913c6edf6d581ee49874f3ac78cc95933240aa5
Justin Morrison <morrisonxjustin@gmail.com>
2025-06-22 22:18:02 -0400

    Updated to have an option to not include the title. It still uses the same colors, but the icon and title can be removed. It takes up less space and is less visual clutter which I may want in the future

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,17 +1,20 @@
 const pop_up_style1 = "border-l-4 p-4 rounded-md my-4 bg-zinc-800"
 const pop_up_style2 = "border-l-4 p-4 my-4 bg-zinc-900"
 
-export default function PopUp({ color, icon, title, children, fontSize, ...props }) {
+export default function PopUp({ color, icon, title, children, fontSize, notitle, ...props }) {
     const Icon = icon;
 
     return (
-        <div className={pop_up_style1} style={{ borderColor: color }}>
-            <div className='flex gap-2 items-center mb-2' style={{ color: color, fontSize: fontSize }} >
-                {
-                    Icon ? <Icon size={16} /> : null
-                }
-                <strong style={{ color: color }}>{title}</strong>
-            </div>
+        <div className={pop_up_style1 + ` ${notitle ? "py-6" : ""}`} style={{ borderColor: color }}>
+            {!notitle &&
+
+                <div className='flex gap-2 items-center mb-2' style={{ color: color, fontSize: fontSize }} >
+                    {
+                        Icon ? <Icon size={16} /> : null
+                    }
+                    <strong style={{ color: color }}>{title}</strong>
+                </div>
+            }
 
             {children}
         </div>
