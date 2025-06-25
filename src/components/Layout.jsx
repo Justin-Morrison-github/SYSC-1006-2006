2cca365ce5f5b462630ae3c1e3d30e102702ae94
Justin Morrison <morrisonxjustin@gmail.com>
2025-06-25 01:14:51 -0400

    Hide horizontal sidebar on main page since it doesn't look good.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Sidebar from './Sidebar';
 export default function Layout({ children }) {
     const [sidebarOpen, setSidebarOpen] = useState(false)
     return (
-        <div className="relative min-h-screen bg-gray-950 text-white">
+        <div className="relative min-h-screen bg-gray-950 text-white overflow-x-hidden">
             <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
             <main className={`relative pt-12 ml-0 transition-all ${sidebarOpen ? "left-[260px]" : "left-0"}`}>
                 {children}
