69c2b05969c13ec8975c0f432d47d7a6fca5ddbf
Justin Morrison <morrisonxjustin@gmail.com>
2025-07-07 00:22:49 -0400

    Fixed issue where page contents would overflow on the right when the sidebar was open and overflow was not hidden, which I needed for another component. Fixed now.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,13 @@ import Sidebar from './Sidebar';
 export default function Layout({ children }) {
     const [sidebarOpen, setSidebarOpen] = useState(false)
     return (
-        <div className="relative min-h-screen bg-gray-950 text-white overflow-x-hidden">
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-            <main className={`relative pt-12 ml-0 transition-all ${sidebarOpen ? "left-[260px]" : "left-0"}`}>
+        <div className="flex min-h-screen bg-gray-950 text-white transition-all">
+            <div
+                className={`transition-all duration-200 ${sidebarOpen ? "w-[260px]" : "w-0"}`}
+            >
+                <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            </div>
+            <main className="flex-1 pt-12">
                 {children}
             </main>
         </div>
