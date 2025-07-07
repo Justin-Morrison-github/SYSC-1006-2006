4b670fb2bd4c6e0028cc3ca5867d95c27633c15b
Justin Morrison <morrisonxjustin@gmail.com>
2025-07-07 00:14:20 -0400

    Updated to display title if provided. I like it for I am making mock exams. Fixed but when progress bar was clipping through parent. Slightly changed layout, now progress and checkmark are all the way to the right.

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -6,7 +6,7 @@ import checkmarkAnimation from '../assets/animations/checkmark.json'
 const pop_up_style1 = "border-l-4 p-4 my-8 bg-zinc-800 rounded-md"
 const pop_up_style2 = "border-l-4 p-4 my-8 bg-zinc-900"
 
-export default function Exercise({ children, exercisenumber, dropdown = true, initialCollapsed = false, color, }) {
+export default function Exercise({ children, exercisenumber, dropdown = true, initialCollapsed = false, color, title }) {
     const [collapsed, setCollapsed] = useState(initialCollapsed);
     const [correctMap, setCorrectMap] = useState({});
 
@@ -25,52 +25,53 @@ export default function Exercise({ children, exercisenumber, dropdown = true, in
     const correctCount = Object.values(correctMap).filter(Boolean).length;
     const progressPercent = totalCount === 0 ? 0 : (correctCount / totalCount) * 100;
 
-
     return (
         <div className={pop_up_style1} style={{ borderColor: color }}>
             <div className='sticky top-12 h-12 bg-zinc-800 z-[40]' style={{ color: color, fontSize: "x-large" }} >
-                <button onClick={() => setCollapsed((prev) => !prev)} disabled={!dropdown}>
-                    {/* <div className='flex gap-4 items-center'> */}
-                    <div className='grid grid-cols-[26px_160px_70px_200px_50px] gap-4 items-center text-left'>
-                        <QuestionIcon size={26} />
-                        <strong>Exercise Set {exercisenumber}</strong>
-                        <div className='text-white'>
-                            {correctCount} / {totalCount}
-                        </div>
+                <button onClick={() => setCollapsed((prev) => !prev)} disabled={!dropdown} className='w-full'>
+                    <div className='flex gap-4 items-center justify-between w-full'>
+                        <div className='flex items-center gap-4'>
 
-                        <div className="h-5 w-[200px] bg-gray-300 rounded-md">
-                            {/* Progress indicator */}
+                            <QuestionIcon size={26} />
+                            <strong>{title ?? `Exercise Set ${exercisenumber}`}  </strong>
+                        </div>
+                        <div className='flex items-center gap-4'>
+
+                            <div className='text-white'>
+                                {correctCount} / {totalCount}
+                            </div>
+                            <div className="h-5 w-[200px] bg-gray-300 rounded-md overflow-hidden">
+                                {/* Progress indicator */}
+                                {
+                                    correctCount === totalCount ? (
+                                        <div
+                                            className="h-full bg-green-500 transition-all duration-300 ease-in-out"
+                                            style={{
+                                                width: `${progressPercent}%`,
+                                                boxShadow: '0 0 5px rgb(37, 121, 68), 0 0 10px rgb(55, 156, 91), 0 0 15px rgb(50, 115, 75)',
+                                            }}
+                                        />
+                                    ) : (
+                                        <div
+                                            className="h-full bg-green-500 transition-all duration-300 ease-in-out rounded-md"
+                                            style={{ width: `${progressPercent}%` }}
+                                        />
+                                    )
+                                }
+                            </div>
                             {
                                 correctCount === totalCount ? (
-                                    <div
-                                        className="h-full bg-green-500 transition-all duration-300 ease-in-out rounded-md"
-                                        style={{
-                                            width: `${progressPercent}%`,
-                                            boxShadow: '0 0 5px rgb(37, 121, 68), 0 0 10px rgb(55, 156, 91), 0 0 15px rgb(50, 115, 75)',
-                                        }}
+                                    <Lottie
+                                        animationData={checkmarkAnimation}
+                                        autoplay={true}
+                                        loop={false}
+                                        style={{ height: "3rem" }}
                                     />
                                 ) : (
-                                    <div
-                                        className="h-full bg-green-500 transition-all duration-300 ease-in-out rounded-md"
-                                        style={{ width: `${progressPercent}%` }}
-                                    />
+                                    <div className='h-[48px] w-[48px]'></div>
                                 )
                             }
                         </div>
-
-                        {
-                            correctCount === totalCount ? (
-                                <Lottie
-                                    animationData={checkmarkAnimation}
-                                    autoplay={true}
-                                    loop={false}
-                                    style={{ height: "3rem" }}
-                                />
-                            ) : (
-                                <div className='h-[48px]'></div>
-                            )
-
-                        }
                     </div>
                 </button>
             </div>
